43101513e7725f9c7a8906d03f0717638c77593c
anothersunirsee <fajarahnf@gmail.com>
2026-01-11 19:07:26 +0700

    debug: expose raw error details in create-order api

--- api/create-order.ts
+++ api/create-order.ts
@@ -89,7 +89,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
         return res.status(500).json({
             error: message,
-            details: error.toString()
+            details: error
         });
     }
 }
